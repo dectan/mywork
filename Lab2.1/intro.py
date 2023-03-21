21f027583cd313a51eab0bf1a51c1016256adc29
Tannian <Declan.Tannian@tranetechnologies.com>
2023-03-21 13:42:11 +0000

    week 02

---
+++ Lab2.1/intro.py
@@ -0,0 +1,33 @@
+number = int(input('please enter for addition query:'))
+newNumber = number + 1
+print (f'{number} plus 1 is {newNumber}')
+
+number = int(input('please enter for subtraction query:'))
+newNumber = number -8
+print (f'{number} minus 8 is {newNumber}')
+
+number = int(input('please enter for multiplication query:'))
+newNumber = number * 55
+print (f'{number} multplied by 55 is {newNumber}')
+
+number = int(input('please enter for division /decimal:'))
+newNumber = number /4
+print (f'{number} divided by 4 is {newNumber}')
+
+number = int(input('please enter for division /integer:'))
+newNumber = number /4
+print (f'{number} plus one is', int(newNumber))
+
+number = int(input('please enter 2 to calculate for power of 8 :'))
+newNumber = number ** 8
+print (f'{number} to power of 8 is', int(newNumber))
+
+a = 3
+b = 3
+if b > a:
+  print("b is greater than a")
+
+a = 3
+b = 3
+if b >= a:
+  print("b is greater than or equal to a")
